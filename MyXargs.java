c09918ea1bdf37b61a96679587183ae6be7232b0
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2024-09-17 10:43:06 -0700

    'final' commit

--- MyXargs.java
+++ MyXargs.java
@@ -19,12 +19,9 @@ public class MyXargs {
 
         List<String> baseArgs = new ArrayList<>();
 
-        List<String> baseArgs = new ArrayList<>();
-
         // Verifies there are arguments for the program
         if (args.length == 0) {
             System.out.println("Usage: java MyXargs.java [-n num] [-I replace] [-t] [-r] command");
-            System.exit(0);
             System.exit(0);
         }
 
@@ -38,8 +35,6 @@ public class MyXargs {
                 line = reader.readLine();
                 //System.out.println("line: " + line);
                 input.append(line).append(" ");     // Append each line to input with a space separator
-                System.out.println("line: " + line);
-                input.append(line).append(" "); // Append each line to input with a space separator
             }
         }   catch (IOException e) {
             System.out.println("Error reading input: " + e.getMessage());
@@ -64,33 +59,20 @@ public class MyXargs {
             System.out.println("args at " + i + ": " + args[i]);
         }
         */
-        
-
 
         // Determines flags and logic
         // StringBuilder baseCmd = new StringBuilder("");
         for (int i = 0; i < args.length; ++i) {
             switch (args[i]) {
-        // Determine flags and logic
-        //StringBuilder baseCmd = new StringBuilder("");
-        for (int i = 0; i < args.length; ++i) {
-            switch (args[i]) {
                 case "-n":
                 nFlag = true;
                 if (isInt(args[i+1])) {maxArgs = Integer.parseInt(args[i + 1]); } // Assigns int value of next arg to maxArgs variable.
-                if (isInt(args[i+1])) {maxArgs = Integer.parseInt(args[i + 1]); } // Assigns int value of next arg to maxArgs variable.
-                
                 break;
 
-                case "-I":
                 case "-I":
                 iFlag = true;
                 placeholder = args[i + 1];
                 // System.out.println("placeholder: " + placeholder); // Tests if expected placeholder is printed
-                placeholder = args[i + 1];
-                System.out.println("placeholder: " + placeholder);
-                System.out.println("args[i + 1]" + args[i + 1]);
-                System.out.println("blabalsdasdjhlsadjlsakdjlaskdjlsakjdlsajd");
                 break;
 
                 case "-t":
@@ -101,22 +83,12 @@ public class MyXargs {
                 rFlag = true;
                 break;
 
-                case "-outputFormat":
-                break;
-
                 default: 
                 if (args[i].startsWith("-")) {
                     break; // Ignore unrecognized flags
                 } if (args[i] == placeholder) {break;}
                 if (isInt(args[i])) { break; }
                 baseArgs.add(args[i]);
-                //System.out.println("arg parser: " + baseCmd); // TODO: Remove Testing method
-                if (args[i].startsWith("-")) {
-                    break; // Ignore unrecognized flags
-                }
-                if (isInt(args[i])) { break; }
-                baseArgs.add(args[i]);
-                //baseCmd.append(args[i]);
                 //System.out.println("arg parser: " + baseCmd); // TODO: Remove Testing method
             }
         }
@@ -125,13 +97,6 @@ public class MyXargs {
         //System.out.println("Base Command: " + baseCmd);       // Outputs command that program runs (i.e. rm, echo, etc.)
         //System.out.println("placeholder: " + placeholder);    // Outputs placeholder for -I testing
         nFlag = true; 
-
-        System.out.println("iflag is: "+ iFlag);
-        System.out.println("baseArgs: " + baseArgs);
-        String baseCmd = String.join(" ", baseArgs);
-        System.out.println("Base command: " + baseCmd);
-        System.out.println("placeholder: " + placeholder);
-        nFlag = true; // 
 
         if (rFlag) {
             if (inputList.length == 1) {
@@ -143,9 +108,6 @@ public class MyXargs {
         // Initialize base command (baseCmd)
         List <String> commandList = new ArrayList<String>();
 
-       
-       
-        String finalCmd = baseCmd.toString();
        if (maxArgs > inputList.length) {
         maxArgs = inputList.length;
        }
@@ -155,11 +117,6 @@ public class MyXargs {
                 int cmdCounter = 0;
                 //if (nFlag && maxArgs >= inputList.length) {
                     finalCmd = baseCmd.replace(placeholder, "");
-                    int endIndex;
-                    if (i + maxArgs > inputList.length) {
-                finalCmd = baseCmd.toString().replace(placeholder,inputList[i]);
-            }
-            if (nFlag) {
                     int endIndex;
                     if (i + maxArgs > inputList.length) {
                         endIndex = inputList.length;
@@ -191,26 +148,10 @@ public class MyXargs {
                     
                     commandList.add(newCmd.toString());
                 i += maxArgs - 1;
-                    } else {endIndex = i + maxArgs;}
-                    StringBuilder newCmd = new StringBuilder(baseCmd);
-                        System.out.println();
-                        for (int j = i; j < endIndex; j++) {
-                        System.out.println("appending here: ");
-                        System.out.println(newCmd + " " + inputList[j]); // TODO: Tests groupings
-                        newCmd.append(" ").append(inputList[j]);
-                        }
-                    
-                    System.out.println("\nnewCmd: " + newCmd);
-                    commandList.add(newCmd.toString());
-                    System.out.println("Command: " + newCmd.toString());
-                i += maxArgs - 1;
             }
             else {
                 String cmd = finalCmd + " " + inputList[i];
                 commandList.add(cmd);
-                String cmd = baseCmd + " " + inputList[i];
-                commandList.add(cmd);
-                System.out.println("Command: " + cmd); // Test to print command groupings
             }
         }
         /* 
@@ -227,27 +168,8 @@ public class MyXargs {
 
             if (tFlag) {System.out.println("+ " + cmd);} // Outputs command before executing
 
-
-
-        System.out.println("commandList: ");
-        for (String cmd: commandList) {
-            System.out.println("cmd: " + cmd);
-        }
-
-            // Executes all commands in the commandList
-            String ex = " pb command not set"; // Shows Work in Progress
-            ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "echo" + ex); // Default to echo cmd
-            int l = 0;
-            pb.inheritIO(); // Make the subprocess use the same I/O as the parent process
-
-            System.out.println("Command output: "); // TODO: Remove testing print
-            // For each cmd in the commandList, update the pb cmd and execute
-            for (String cmd : commandList) {
-                pb.command("cmd.exe", "/c", cmd);            // Updates the command that pb will run
-                if (tFlag) {System.out.println(cmd);} // Outputs command before executing
             try {
                 Process process = pb.start();
-                process.waitFor();          // Wait for the process to finish
                 process.waitFor();          // Wait for the process to finish
             } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
@@ -279,24 +201,4 @@ public class MyXargs {
                 return false; 
             }
     }
-    } // end of main class
-        }
-
-
-
-    } // end of main method
-    
-    
-    
-    // Checks if the provided string can be converted to an integer
-    public static boolean isInt(String str) {
-            try {
-                Integer.parseInt(str);
-                return true;  
-            } catch (NumberFormatException e) {
-                return false; 
-            }
-        
-    }
-
     } // end of main class
